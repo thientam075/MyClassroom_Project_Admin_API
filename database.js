969d0328939c7b8b58c6bfb2ffd8e0afdc7881d1
thientam075 <72021036+thientam075@users.noreply.github.com>
2022-01-18 18:03:19 +0700

    Update database.js

--- database.js
+++ database.js
@@ -5,14 +5,14 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.e
     dialect: 'postgres',
     port: 5432,
     
-    // dialectOptions: {ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false
-    //     }
-    // },
-    ssl: true,
+    dialectOptions: {ssl: {
+        require: true,
+        rejectUnauthorized: false
+        }
+    },
     
 });
+
 try {
     db.authenticate().then(
         console.log('Connection has been established successfully.')
